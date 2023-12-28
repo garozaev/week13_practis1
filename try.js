506daed257b726d10d4b57a58cfaca446db9f2c4
garozaev <g.a.rozaev@gmail.com>
2023-12-28 22:15:37 +0300

    in progress2

---
+++ try.js
@@ -0,0 +1,27 @@
+
+/*const age = 18;
+if (age >= 18) {
+    console.log('Добро пожаловать на выборы');
+};*/
+/*let signal = "";
+if (signal === "red") {
+    console.log("Идти нельзя");
+} else if (signal === "yellow") {
+    console.log("Надо прриготовиться");
+} else {
+    console.log("Можно идти");
+}*/
+
+
+// Перепишите условие используя тернарные операторы
+
+//const num = 11
+//let result = (num > 10) ? 'Число больше 10' : 'Число меньше или равно 10';
+
+/*if (num > 10) {
+    result = 'Число больше 10'
+} else {
+    result = 'Число меньше или равно 10'
+}*/
+
+//console.log(result)
